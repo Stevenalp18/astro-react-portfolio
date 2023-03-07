bcd06248e98d856be18aecd101656dc07b38e5fd
Steven Perez <stevenperez@Stevens-MacBook-Pro.local>
2023-03-06 22:53:57 -0500

    Started new astro + react portfolio

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar"
+
+export default function Header(){
+    return (
+        <header className="bg-[url('public/mountain-background.jpeg')] bg-no-repeat bg-center bg-cover">
+            <Navbar />
+            <div className="mx-6 mt-32 sm:mt-52 sm:w-4/6 max-w-2xl">
+                <p className="rounded-2xl text-2xl leading-[2] sm:p-4 sm:text-5xl sm:leading-[1.4] text-white font-shantell text-gray-100">Hi, My name is Steven Perez and I like to create websites and build random projects!</p>
+            </div>
+            <button className="rounded-xl mx-6 mt-10 my-2 mb-60 p-3 text-md font-medium text-gray-100 bg-slate-50/25 font-alegreya md:mb-72">Contact</button>
+        </header>
+    )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+
+export default function Navbar() {
+
+    return (
+        <nav className="flex justify-between p-4 sm:p-10 sm:text-lg text-white">
+            <div className=''>
+                Steven Perez
+            </div>
+            <ol className="flex gap-6 invisible sm:visible">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#" >Projects</a></li>
+                <li><a href="#" className="px-4 py-3 rounded-lg bg-slate-50/10">Contact</a></li>
+            </ol>
+        </nav>
+    )
+}
+
+//Fix and Make Hamburger at end
