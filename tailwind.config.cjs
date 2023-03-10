df8a8335e6f39732acdc2b19372596600b468c83
Steven Perez <stevenperez@stevens-mbp.myfiosgateway.com>
2023-03-09 21:51:28 -0500

    changed local assets to link to github repo images

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,7 +10,7 @@ module.exports = {
 			},
 			backgroundImage: {
 				'mountain': "url('https://github.com/Stevenalp18/web-dev-images/blob/main/mountain-background.jpeg?raw=true')",
-				'starry-sky': "url('https://github.com/Stevenalp18/web-dev-images/blob/main/starry-night.jpeg?raw=true')"
+				'starry-sky': "url('https://images.unsplash.com/photo-1618890334461-c33a04c4c916?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80')"
 			}
 		},
 	},
